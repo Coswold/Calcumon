5b14ca7d4f61b1d8c578b4e1c8045638cdc3633d
Connor Oswold <coswoldo@gmail.com>
2019-02-07 10:40:08 -0800

    starting auth and building out file structure

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express();
+const port = 3000
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+var exphbs = require('express-handlebars')
+
+
+app.use(cookieParser());
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'))
+
+
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+        req.user = null;
+    } else {
+        var token = req.cookies.nToken;
+        var decodedToken = jwt.decode(token, { complete: true }) || {};
+        req.user = decodedToken.payload;
+    }
+
+    next();
+};
+
+app.use(checkAuth);
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+module.exports = app;
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
