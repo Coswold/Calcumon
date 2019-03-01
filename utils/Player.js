5e97c28eaf79de1de903e9d83c72cd5f0a58fc87
Anisha Jain <anishajain@outlook.com>
2019-03-01 11:58:11 -0800

    onclick functions for calcumons

--- utils/Player.js
+++ utils/Player.js
@@ -4,18 +4,11 @@
 // attacks and attack costs
 // response input to a problem
 
-<<<<<<< HEAD
 // import problem generator
 import { getProblem } from 'problemGenerator'
 
 // import solution API
 import { solve } from 'solutionApi'
-=======
-// import problem generator 
-// import { getProblem } from 'problemGenerator'
-// // import solution API
-// import { solve } from 'solutionApi'
->>>>>>> 4c2a5a9db784e3950b8d250cf9606ffb13cf9e02
 
 class Player {
     constructor(attacks=[], health=100, mana=0, level=1) {
@@ -50,13 +43,8 @@ class Player {
         // update currSolution with new solution
         // console.log(result[0])
         // console.log('GETTING PROBLEM-SOLUTION')
-<<<<<<< HEAD
         this.currSolution = solve(result[0])
         console.log(this.currSolution)
-=======
-        // this.currSolution = solve(result[0])
-        // console.log('this.currSolution')
->>>>>>> 4c2a5a9db784e3950b8d250cf9606ffb13cf9e02
 
         // increment problem count
         this.problemCount += 1
@@ -68,18 +56,7 @@ class Player {
 
     // IMPLEMENT THIS FOR VERSION ONE
     respond() {
-<<<<<<< HEAD
-        // NOTE: CHECK IF WE CAN USE document.getElementById in this file to access gamePlay.handlebars!
-        // let submit = document.getElementById('submit')
-        // let userInput = null
-        // submit.onclick = function() {
-        //     userInput = document.getElementById('userInput').value
-        //     alert(userInput)
-        // }
-        // return userInput
-=======
         // currently handled in game.js
->>>>>>> 4c2a5a9db784e3950b8d250cf9606ffb13cf9e02
     }
 
     // Returns the health value that needs to be subtracted from opponent
@@ -115,11 +92,7 @@ class Player {
 
 }
 
-<<<<<<< HEAD
 player = new Player()
 player.newProblem()
 
 //export default Player
-=======
-// export default Player
->>>>>>> 4c2a5a9db784e3950b8d250cf9606ffb13cf9e02
