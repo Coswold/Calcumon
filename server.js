d4f0c39f1a76b41eec5e196b0c3a526f3ccee25c
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-03-02 17:47:20 -0800

    small change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
-require('dotenv').config();
+require('dotenv').load();
 process.env.SECRET
 
 const bodyParser = require('body-parser');
