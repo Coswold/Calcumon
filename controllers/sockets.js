6241de9ddfa43ea895844ba31a50020811ddf1f5
Anisha Jain <anishajain@outlook.com>
2019-03-22 15:50:00 -0700

    Solution verification and socket communication implemented

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -3,6 +3,16 @@ module.exports = (app, io) => {
     var rooms = 0;
 
     io.on('connection', function(socket) {
+        // NOTE: cleint side sends in username when game is initialized using 
+                    // 'socket.emit('add user', username)' 
+        // I believe this can be accessed as data.username
+        // reference: https://github.com/socketio/socket.io/blob/master/examples/chat/public/main.js
+
+        // NOTE: client side sends in a bool value when solution is submitted using: 
+                    // socket.emit('solution submitted', [sol, username])
+
+        // NOTE: client side EXPECTS server side to send health updates as [username, health]. Accessed as follows : 
+                    // socket.on('health update', function(msg) {...})
 
         socket.on('createGame', function(data){
             let player1 = 100;
@@ -10,7 +20,6 @@ module.exports = (app, io) => {
             socket.join('room-' + ++rooms);
             socket.emit('newGame', {'player1': player1, 'player2': player2, room: 'room-'+rooms});
         });
-
 
         socket.on('answer submission', function(data) {
             if (data.player == "player1") {
