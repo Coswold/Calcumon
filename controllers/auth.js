f47760fe13b5c2c2b0642780e5c4af1dcc0bfbfc
Anisha Jain <anishajain@outlook.com>
2019-03-25 13:53:40 -0700

    choose

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,7 +20,7 @@ module.exports = (app) => {
         console.log(user)
         user.save()
         .then((user) => {
-            var token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "60 days" });
+            var token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, { expiresIn: "60 days" });
             res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
             res.redirect("/choose");
         })
--- controllers/choose.js
+++ controllers/choose.js
@@ -4,24 +4,32 @@ const Calcumon = require('../models/calcumon')
 module.exports = (app) => {
 
     app.get('/choose', (req, res) => {
-        res.render('chooseCalcumon');
+        let username = "test"
+        if (req.user) {
+            username = req.user.username
+        }
+        res.render('chooseCalcumon', { username: username});
     });
 
     // CREATE
     app.post('/choose/calcumon', (req, res) => {
+        console.log(req.body)
         // INSTANTIATE INSTANCE OF CALCUMON MODEL
+        if (req.body.user) {
+            req.user = req.body.user
+        }
         if (req.user) {
-            const calcumon = new Calcumon(req.body);
+            const calcumon = new Calcumon({ name: req.body.calcumon });
          // SAVE INSTANCE OF CALCUMON MODEL TO DB
          calcumon.save().then(calcumon => {
-             return User.findById(req.user._id);
+             return User.findOne({username: req.body.user});
          })
          .then(user => {
              user.monster = calcumon.name;
              console.log(user.monster)
              user.save();
              // REDIRECT TO THE DASHBOARD
-             res.redirect(`/dashboard/${user._id}`)
+             res.status(200).send(`/dashboard/${user._id}`)
          })
          .catch(err => {
              console.log(err.message);
